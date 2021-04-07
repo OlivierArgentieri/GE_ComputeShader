83c4bccae89721203c12469186556ea2a4782156
OlivierArgentieri <olivierargentieri@gmail.com>
2021-04-07 11:34:03 +0200

    add basic imgui

--- GameEngine/src/Main.cpp
+++ GameEngine/src/Main.cpp
@@ -13,6 +13,7 @@
 #include "Log.h"
 #include "Timer.h"
 #include "Window.h"
+
 #include "engine/loaders/obj/obj_loader.h"
 LogConfig LOG_CONFIG = {};
 
@@ -32,108 +33,49 @@ typedef struct _RGBA_ {
 
 int main(void)
 {
-	const int SCREEN_WIDTH = 800;
-	const int SCREEN_HEIGHT = 640;
+	const int SCREEN_WIDTH = 1600;
+	const int SCREEN_HEIGHT = 1280;
 
 	// Init Logging
 	// Log init
 	LOG_CONFIG.reporting_level = Debug;
 	LOG_CONFIG.restart = true;
 	if (LOG_CONFIG.restart) { Log::restart(); }
-
 	
 	// Init Window
-	/**/Window _window = Window("Hello framework");
+	/**/Window _window = Window("openGL window");
 	if (!_window.Init(SCREEN_WIDTH, SCREEN_HEIGHT))
 	{
 		return 1;
 	}
 
-	
-	
 	// Init Game
 	Timer _timer;
 	Game _game;
 	_game.Init(SCREEN_WIDTH, SCREEN_WIDTH);
 	_game.Load();
 
-
-	// test load obj
-	std::vector< glm::vec3 > vertices;
-	std::vector< glm::vec2 > uvs;
-	std::vector< glm::vec3 > normals; // Won't be used at the moment.
-	bool res = loadOBJ("D:/Projet/PullGithub/GE_CustomShader/Bin/Debug/cube.obj", vertices, uvs, normals);
-	
-	/*float _dt = 0;
 	bool show_demo_window = true;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-	while (_game.isRunning && !_window.WindowShouldClose())
-	{
-		_dt = static_cast<float>(_timer.ComputeDeltaTime());
-		_window.UpdateFpsCounter(_dt);
-		_window.UpdateBackgroundColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		
-		//_game.HandleInputs();
-		//_game.Update(_dt);
-
-		_window.ClearBuffer();
-		_window.NewImGUIFrame();
-
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
-		
-
-		// one window
-		{
-			static float f = 0.0f;
-			static int counter = 0;
-
-			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			ImGui::End();
-		}
-
-		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
-		_window.ImGUIRender();
-		//_game.Render();
-		_window.SwapBuffer();
-		_timer.DelayTime();
-		_window.UpdateInputEvent();
-	}*/
+	
 
 	
 	
 	/* ----- Quad Context ----- #1#*/
-	RGBAValues* quadTexture = new RGBAValues[640 * 480];
 	CoordinatesSet* quadVertex = new CoordinatesSet[4];
 	unsigned short int* quadIndex = new unsigned short int[3];
 
-	quadVertex[0].x = -1.0f;
-	quadVertex[0].y = 1.0f;
+	quadVertex[0].x = -0.5f;
+	quadVertex[0].y = 0.5f;
 
-	quadVertex[1].x = -1.0f;
-	quadVertex[1].y = -1.0f;
+	quadVertex[1].x = -0.5f;
+	quadVertex[1].y = -0.5f;
 
-	quadVertex[2].x = 1.0f;
-	quadVertex[2].y = 1.0f;
+	quadVertex[2].x = 0.5f;
+	quadVertex[2].y = 0.5f;
 
-	quadVertex[3].x = 1.0f;
-	quadVertex[3].y = -1.0f;
+	quadVertex[3].x = 0.5f;
+	quadVertex[3].y = -0.5f;
 
 	quadIndex[0] = 0;
 	quadIndex[1] = 1;
@@ -148,15 +90,6 @@ int main(void)
 	quadVertex[2].t = 0.0f;
 	quadVertex[3].s = 1.0f;
 	quadVertex[3].t = 1.0f;
-
-	for (int x = 0; x < 640; x++) {
-		for (int y = 0; y < 480; y++) {
-			quadTexture[x + y * 640].Red = 1.0f;
-			quadTexture[x + y * 640].Green = .5f;
-			quadTexture[x + y * 640].Blue = .0f;
-			quadTexture[x + y * 640].Alpha = 1.0f;
-		}
-	}
 
 
 	/* ----- Render Context ----- #1#*/
@@ -177,7 +110,7 @@ int main(void)
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, 640, 480, 0, GL_RGBA, GL_FLOAT, NULL);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	glGenBuffers(1, &quadIBO);
+	/**/glGenBuffers(1, &quadIBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadIBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(unsigned short int), quadIndex, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -199,7 +132,7 @@ int main(void)
 	glEnableVertexAttribArray(0);
 
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)(sizeof(GLfloat) * 2));
-	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(1);/**/
 
 	glBindVertexArray(0);
 
@@ -235,7 +168,7 @@ int main(void)
 
 	/* ----- Run Compute shader ----- #1# */
 	_computeShader.Use();
-	glBindTexture(GL_TEXTURE_2D, quadTextureID);
+	/**/glBindTexture(GL_TEXTURE_2D, quadTextureID);
 	glBindImageTexture(0, quadTextureID, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 	glDispatchCompute(40, 30, 1);
 	glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
@@ -245,36 +178,88 @@ int main(void)
 
 
 	/* ----- Render loop ----- #1# */
-	while (true) {
 
-		glfwPollEvents();
+	float _dt;
+	while (_game.isRunning && !_window.WindowShouldClose())
+	{
+		_window.UpdateInputEvent();
+		
+		_dt = static_cast<float>(_timer.ComputeDeltaTime());
+		_window.UpdateFpsCounter(_dt);
+		_window.UpdateBackgroundColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
 
-		Sleep(40);
+		//_game.HandleInputs();
+		//_game.Update(_dt);
 
-		glEnable(GL_CULL_FACE);
+		_window.ClearBuffer();
+		_window.NewImGUIFrame();
 
-		/* ----- Actual render ----- #1# */
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 
 		glUseProgram(programID);
-
 		glBindVertexArray(quadVAO);
 
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, quadTextureID);
+		//glBindTexture(GL_TEXTURE_2D, quadTextureID);
 
-		glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, (GLvoid*)0);
+		//glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, (GLvoid*)0);
 
-		glBindTexture(GL_TEXTURE_2D, 0);
-		glActiveTexture(0);
+		// one window
+		ImGui::BeginMainMenuBar();
 
-		glBindVertexArray(0);
+		ImGui::Text("test");
+		ImGui::EndMainMenuBar();
+		
+		
+		ImGui::Begin(("Shader_one"));
+		{
+			ImGui::BeginChild("Shader_one");
+			{
+				ImVec2 wsize = ImGui::GetWindowSize();
+				ImGui::Image((ImTextureID)quadTextureID, wsize, ImVec2(0, 1), ImVec2(1, 0));
+				ImGui::EndChild();
+				// Create a window called "Hello, world!" and append into it.
 
-		glUseProgram(0);
+				ImGui::Text("Shader Text");               // Display some text (you can use a format strings too)
 
+
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				
+			}
+			ImGui::End();
+		}
+
+		ImGui::Begin(("Shader_two"));
+		{
+			ImGui::BeginChild("Shader_two");
+			{
+				ImVec2 wsize = ImGui::GetWindowSize();
+				ImGui::Image((ImTextureID)quadTextureID, wsize, ImVec2(0, 1), ImVec2(1, 0));
+				ImGui::EndChild();
+				// Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("Shader Text");               // Display some text (you can use a format strings too)
+
+
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				
+			}
+			ImGui::End();
+		}
+		
+		_window.ImGUIRender();
+		//_game.Render();
 		_window.SwapBuffer();
+		_timer.DelayTime();
+		
 	}
-
 	return 0;
-
 }
--- GameEngine/src/game/Game.cpp
+++ GameEngine/src/game/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include "Texture.h"
+#include "../engine/loaders/obj/obj_loader.h"
 
 Game::Game() : isRunning(false), windowWidth(0), windowHeight(0)
 {}
@@ -14,10 +15,14 @@ void Game::Init(int _screenWidth, int _screenHeight)
     isRunning = true;
 }
 
-
 void Game::Load()
 {
-	
+    // test load obj
+    /**/
+	std::vector< glm::vec3 > vertices;
+    std::vector< glm::vec2 > uvs;
+    std::vector< glm::vec3 > normals; // Won't be used at the moment.
+    bool res = loadOBJ("D:/Projet/PullGithub/GE_CustomShader/Bin/Debug/cube.obj", vertices, uvs, normals);
 }
 
 
@@ -51,7 +56,7 @@ void Game::Update(float _dt)
 {
     /* if()
          speed =-speed;
-     */
+     
 
     if (lastPosition > 1.0f)
     {
@@ -62,7 +67,7 @@ void Game::Update(float _dt)
         speed = -speed;
     }
     matrix[12] = speed * _dt + lastPosition;
-    lastPosition = matrix[12];
+    lastPosition = matrix[12];*/
     shader.Use();
     int matrixLocation = glGetUniformLocation(shader.GetProgramID(), "matrix");
     glUniformMatrix4fv(matrixLocation, 1, GL_FALSE, matrix);
