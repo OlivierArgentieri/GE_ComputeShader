fc010fd36b0fcc91ea5c82d3ac4222ba5a607186
OlivierArgentieri <olivierargentieri@gmail.com>
2021-04-08 19:01:29 +0200

    ok first bind to imgui window

--- GameEngine/src/game/shader_scene/obj_scene/ObjScene.cpp
+++ GameEngine/src/game/shader_scene/obj_scene/ObjScene.cpp
@@ -52,12 +52,61 @@ void ObjScene::Init()
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
 
+
+	
+
+
 	
 }
 
 void ObjScene::Update(float _dt, glm::mat4 _mvp)
 {
+	/* create to texture to render */
+	GLuint _idTexture;
+	glGenTextures(1, &_idTexture);
 
+	// Binding de la texture pour pouvoir la modifier.
+	glBindTexture(GL_TEXTURE_2D, _idTexture);
+
+	// Création de la texture 2D vierge de la taille de votre fenêtre OpenGL
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1024, 768, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+	// Paramètrage de notre texture (étirement et filtrage)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+
+	/* create FBO */
+	glGenFramebuffers(1, &fbo);
+
+	// On bind le FBO
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+	// Affectation de notre texture au FBO
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, _idTexture, 0);
+
+	// Affectation d'un drawbuffer au FBO
+	GLenum DrawBuffers[2] = { GL_COLOR_ATTACHMENT0 };
+	glDrawBuffers(1, DrawBuffers);
+
+
+	// render to texture
+	// Activation et binding la texture
+	glBindTexture(GL_TEXTURE_2D, _idTexture);
+	glActiveTexture(GL_TEXTURE0);
+
+	// Activation du FBO
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+	glViewport(0, 0, 1024, 768);
+
+	// Changement de la couleur de background
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	// Rafraichissement des buffers (reset)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// =================================================================
+
+	
 	//framgentShader.Use(); // todo static method
 	glUseProgram(programID);
 
@@ -98,9 +147,14 @@ void ObjScene::Update(float _dt, glm::mat4 _mvp)
 	);
 	glDrawArrays(GL_TRIANGLES, 0, vertices.size());
 
+	// =================================================================
+
+
+	
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	
 	ImVec2 wsize = ImGui::GetWindowSize();
-	ImGui::Image(reinterpret_cast<ImTextureID>(fbo), wsize, ImVec2(0, 1), ImVec2(1, 0));
+	ImGui::Image(reinterpret_cast<ImTextureID>(_idTexture), wsize, ImVec2(0, 1), ImVec2(1, 0));
 
 	ImGui::Text("Shader Text");               // Display some text (you can use a format strings too)
 	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
